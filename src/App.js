7b21d7afd007c8495d94cfe387c26a477b259210
dsh410 <dsh410@gmail.com>
2025-06-30 18:31:37 -0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {  
+  BrowserRouter as Router,
+  Routes,
+  Route 
+} from 'react-router-dom';  
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className=" text-red-500" >hello, world!</h1>
-    </div>
+   <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes>
+    </Router>
+   </>
   );
 }
 
