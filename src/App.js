217e5d37f00e64fa8a90f24263df60bd6be6c1f9
dsh410 <dsh410@gmail.com>
2025-07-02 08:14:47 -0500

    replaced navbar with header

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
-import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 function App() {
   return (
    <>
     <Router>
-      <NavBar/>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/profile" element={<Profile />} />
